9f7bb32d5f67a9c24f451f6fdec391fec3ac72b6
AliRezvaniAra <ali.rezvaniara@gmail.com>
2022-01-19 13:19:54 +0330

    initial

---
+++ js/init.js
@@ -0,0 +1,26 @@
+$('.owl-carousel1').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    rtl: true,
+})
+$('.owl-carousel2').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    rtl: true,
+})
+$('.owl-carousel3').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    rtl: true,
+    dots: false
+})
+$('.owl-carousel4').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    rtl: false,
+    dots: false
+})
